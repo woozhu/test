1baf96f52956d7989eee7f8f70a4ceaa01a62514
VITOWU <552198+woozhu@users.noreply.github.com>
2018-10-12 15:39:54 +0800

    Update wxStruct.go

--- wxStruct.go
+++ wxStruct.go
@@ -30,13 +30,64 @@ type SiXiang struct{
     name string
     cname string
 }
+func (sx *SiXiang) String() string{
+   str:=fmt.Sprintf("id=[%d] cname=[%s]",sx.id,sx.cname)
+   return str
+}
+//构造函数
+func newSiXiang(id int,name string,cname string) SiXiang{
+    return SiXiang{id=id,name=name,cname=cname}
+}
+//集成taiji的xchange接口
+func (sx *SiXiang) xchange() SiXiang{
+    switch sx.id{
+        case 0{
+            return newSiXiang(1,"ShaoYang","少阳")
+        }
+        case 1{
+            return newSiXiang(2,"LaoYang","老阳")
+        }
+        case 2{
+            return newSiXiang(3,"ShaoYin","少阴")
+        }
+        case 3{
+            return newSiXiang(0,"LaoYin","老阴")
+        }
+        default return nil
+    }
+}
 
 type WuXing struct{
     id int
     name string
     cname string
 }
-
+func (wx *WuXing) String() string{
+   str:=fmt.Sprintf("id=[%d] cname=[%s]",wx.id,wx.cname)
+   return str
+}
+//构造函数
+func newWuXing(id int,name string,cname string) WuXing{
+    return WuXing{id=id,name=name,cname=cname}
+}
+//集成taiji的xchange接口
+func (wx *WuXing) xchange() WuXing{
+    switch wx.id{
+        case 0{
+            return newSiXiang(1,"ShaoYang","少阳")
+        }
+        case 1{
+            return newSiXiang(2,"LaoYang","老阳")
+        }
+        case 2{
+            return newSiXiang(3,"ShaoYin","少阴")
+        }
+        case 3{
+            return newSiXiang(0,"LaoYin","老阴")
+        }
+        default return nil
+    }
+}
 type TianGan struct{
     id int
     name string
