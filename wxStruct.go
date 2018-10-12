3a4d5aa4a56a49f9db7f07e268fa2fcf8e097327
VITOWU <552198+woozhu@users.noreply.github.com>
2018-10-12 14:50:27 +0800

    Update wxStruct.go

--- wxStruct.go
+++ wxStruct.go
@@ -12,15 +12,44 @@ func (y *YinYang) String() string{
    str:=fmt.Sprintf("id=[%d] cname=[%s]",y.id,y.cname)
    return str
 }
-
+//构造函数
 func newYinYang(id int,name string,cname string) YinYang{
     return YinYang{id=id,name=name,cname=cname}
 }
-
+//集成taiji的xchange接口
 func (y *YinYang) xchange() YinYang{
     if y.id == 0{
-    return newYinYang(1,"Yang","阳")
+        return newYinYang(1,"Yang","阳")
     }else{
-    return newYinYang（0,"Yin","阴"）
+        return newYinYang(0,"Yin","阴")
     }
+}                     
+
+type SiXiang struct{
+    id int
+    name string
+    cname string
+}
+
+type WuXing struct{
+    id int
+    name string
+    cname string
+}
+
+type TianGan struct{
+    id int
+    name string
+    cname string
+    wx WuXing
+    yy YinYang
+}
+
+type DiZhi struct{
+    id int
+    name string
+    cname string
+    wx WuXing
+    yy YinYang
+    canggan []TianGan
 }
