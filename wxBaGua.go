d0a260031ea635bfc114fa77bc3635be7b311d0c
VITOWU <552198+woozhu@users.noreply.github.com>
2018-12-29 18:14:07 +0800

    Update wxBaGua.go

--- wxBaGua.go
+++ wxBaGua.go
@@ -1,3 +1,25 @@
 package wxBaGua
 
-import strconv
+import “strconv”
+
+var ShuXuList=[]int{}
+func List2String(sxl []int) string{
+    var s string
+    for _,i:= range(sxl){
+        s += strconv.ItoA(i)
+    }
+    return s
+}
+
+func String2List(s string) []int{
+    var l=[]int{}
+    for _,i:=range(s){
+         l= append(l,strconv.AtoI(i))
+    }
+    return l
+}
+
+func List2Int(sxl []int) int{
+    s:=List2String(sxl)
+    return int(strconv.AtoI(s))
+}
