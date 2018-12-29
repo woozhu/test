a18274e2f59deb7883c2d5d370ac21cb90363bea
VITOWU <552198+woozhu@users.noreply.github.com>
2018-12-29 18:22:17 +0800

    Update wxBaGua.go

--- wxBaGua.go
+++ wxBaGua.go
@@ -2,7 +2,11 @@ package wxBaGua
 
 import “strconv”
 
+//g关于数序的一些转化函数。
+
 var ShuXuList=[]int{}
+
+//列表转化为字符串
 func List2String(sxl []int) string{
     var s string
     for _,i:= range(sxl){
@@ -10,7 +14,7 @@ func List2String(sxl []int) string{
     }
     return s
 }
-
+//字符串转化为列表
 func String2List(s string) []int{
     var l=[]int{}
     for _,i:=range(s){
@@ -18,12 +22,12 @@ func String2List(s string) []int{
     }
     return l
 }
-
+//列表转化为数字
 func List2Int(sxl []int) int{
     s:=List2String(sxl)
     return int(strconv.AtoI(s))
 }
-
+//数字转化为列表
 func Int2List(i int) []int {
     var l=[]int{}
     for _,ii:=range(strconv.ItoA(i)){
